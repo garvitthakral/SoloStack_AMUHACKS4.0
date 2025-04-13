b4c452f46d9d8897d2953e97bfca9adb871eac50
GARVIT THAKRAL <garvitthakral1001@gmail.com>
2025-04-14 04:51:57 +0530

    Update axios.js

--- StyleShare/src/api/axios.js
+++ StyleShare/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3002",
+  baseURL: "https://solostack-amuhacks4-0-backend.onrender.com",
   withCredentials: true,
 });
 
